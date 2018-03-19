51dc242b99d0ffc81370bb09abbc531bf4365d28
Alan Marchiori <amm042@amm042>
2018-03-19 10:52:04 -0400

    added log message

--- 27-pizzaapi/pizza-hello/pizza.js
+++ 27-pizzaapi/pizza-hello/pizza.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  //console.log(req.method, 'on', req.url, 'from', req.connection.remoteAddress)
+  res.send('Hello World!')
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
